0254cd1a930b44ae6b4f39d673db2c9e5617b0ae
Joe <jjlabajo@newalchemysolutions.com>
2021-11-10 12:00:34 +0800

    add bmi category

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function getBMI(w,h){
 function printDetails(details){
     let details_markup = details.map(function(detail){
         return `
-            <div class="col-sm-4">
+            <div class="col-sm-4 mb-3">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">${detail['Name']}</h5>
@@ -17,6 +17,7 @@ function printDetails(details){
                         <p class="card-text"><b>${columns.w}</b> : &nbsp; ${detail[columns.w]}</p>
                         <p class="card-text"><b>${columns.h}</b> : &nbsp; ${detail[columns.h]}</p>
                         <h6 class="card-subtitle mb-2 text-muted">BMI: ${detail['BMI']}</h6>
+                        <span class="badge bg-${detail['severity']}">${detail['category']}</span>
                     </div>
                 </div>
             </div>
@@ -53,7 +54,25 @@ function upload(that){
                     details.splice(x, 1);
                     continue
                 }
-                details[x]['BMI'] = getBMI(detail[columns.w], detail[columns.h])
+                let bmi = getBMI(detail[columns.w], detail[columns.h])
+                let category = ''
+                let severity = ''
+                if(bmi < 18.5){
+                    category = 'Underweight'
+                    severity = 'danger'
+                }else if(bmi < 25){
+                    category = 'Normal weight'
+                    severity = 'success'
+                }else if(bmi < 30){
+                    category = 'Overweight'
+                    severity = 'warning'
+                }else{
+                    category = 'Obesity'
+                    severity = 'danger'
+                }
+                details[x]['BMI'] = bmi
+                details[x]['category'] = category
+                details[x]['severity'] = severity
             }
             printDetails(details)
         }
